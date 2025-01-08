37f4df23b66dd85de317ca72318d369e493579e7
xforcevesa <nomodeset@qq.com>
2025-01-08 22:11:25 +0800

    first ten

--- exercises/10_trivial/main.cpp
+++ exercises/10_trivial/main.cpp
@@ -5,12 +5,18 @@
 struct FibonacciCache {
     unsigned long long cache[16];
     int cached;
+
+    FibonacciCache() : cached(0) {
+        cache[0] = 0;
+        cache[1] = 1;
+    }
 };
 
 // TODO: 实现正确的缓存优化斐波那契计算
 static unsigned long long fibonacci(FibonacciCache &cache, int i) {
-    for (; false; ++cached) {
-        cache[cached] = cache[cached - 1] + cache[cached - 2];
+    for (int cached = 2; cached <= i; ++cached) {
+        cache.cache[cached] = cache.cache[cached - 1] + cache.cache[cached - 2];
+        cache.cached = cached;
     }
     return cache.cache[i];
 }
